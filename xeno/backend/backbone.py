4cb621674ce375407be7158bd85bcd52dac20f73
unknown <edlafem1@umbc.edu>
2015-03-18 02:53:10 -0400

    edlafem1-flask login commented out, working

--- xeno/backend/backbone.py
+++ xeno/backend/backbone.py
@@ -1,12 +1,34 @@
 from flask import *
+from flask_login import *
+import os
 
 app = Flask(__name__, template_folder='../')
+app.secret_key = os.urandom(24)
+#login_manager = LoginManager()
+#login_manager.init_app(app)
+
+from user_class import *
 
 # Serves main landing page
 @app.route('/')
 def xeno_main():
     return render_template('index.html')
+'''
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    pass
+    form = LoginForm()
+    if form.validate_on_submit():
+        # login and validate the user...
+        username = request.args.get('username', type=str)
+        password = request.args.get('password', type=str)
 
+        user = load_user()
+        login_user(user)
+        flash("Logged in successfully.")
+        return redirect(request.args.get("next") or url_for("xeno_main"))
+    return render_template("login.html", form=form)
+'''
 
 # Test of ajax calls
 @app.route('/ajax_test')
@@ -27,20 +49,23 @@ def ajax_test():
 
 # Allows stylesheets to be loaded.
 # TODO  Consider finding a different way to serve static files without using flask
-#       or to simplify it. i.e. with a 'static' directory
+# or to simplify it. i.e. with a 'static' directory
 @app.route('/style/<sheet>')
 def return_stylesheet(sheet):
     return send_from_directory('../style', sheet)
+
 
 # Allows scripts to be loaded
 @app.route('/scripts/<js>')
 def return_script_file(js):
     return send_from_directory('../scripts', js)
 
+
 # Allows images to be loaded
 @app.route('/images/<image>')
 def return_images(image):
     return send_from_directory('../images', image)
+
 
 # Allows partials to be loaded
 @app.route('/Partials/<partial>')
