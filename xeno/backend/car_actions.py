b1ba6868162cdd1e388bbb7c9a926399f9bfdae6
unknown <edlafem1@umbc.edu>
2015-04-13 21:35:17 -0400

    edlafem1-car listing and add car are done. Car add just needs a few drop downs or autocompletes.

--- xeno/backend/car_actions.py
+++ xeno/backend/car_actions.py
@@ -23,51 +23,46 @@ def get_cars(page, howmany):
     return car_data
 
 def add_new_car(cdata, current_user):
-    '''
-    make, model, year, country, hp(int), torque(int), acceleration(int), max_speed(int)
-    '''
     query = "SELECT id FROM make WHERE description=%s"
     make_id = db_conn.query_db(query, [cdata["make"].lower()], one=True)
     if make_id is None:
         query = "INSERT INTO make (`description`) VALUES (%s)"
         make_id = db_conn.query_db(query, [cdata["make"]], select=False)
-        print "Make id" + str(make_id)
+        #print "Make id" + str(make_id)
     else:
         make_id = int(make_id["id"])
-        print "Make id already there as " + str(make_id)
+        #print "Make id already there as " + str(make_id)
 
     query = "SELECT id FROM model WHERE description=%s"
     model_id = db_conn.query_db(query, [cdata["model"].lower()], one=True)
     if model_id is None:
         query = "INSERT INTO model (`description`) VALUES (%s)"
         model_id = db_conn.query_db(query, [cdata["model"]], select=False)
-        print "Model id" + str(model_id)
+        #print "Model id" + str(model_id)
     else:
         model_id = int(model_id["id"])
-        print "Model id already there as " + str(model_id)
+        #print "Model id already there as " + str(model_id)
 
     query = "SELECT id FROM country WHERE description=%s"
     country_id = db_conn.query_db(query, [cdata["country"].lower()], one=True)
     if country_id is None:
         query = "INSERT INTO country (`description`) VALUES (%s)"
         country_id = db_conn.query_db(query, [cdata["country"]], select=False)
-        print "Country id" + str(country_id)
+        #print "Country id" + str(country_id)
     else:
         country_id = int(country_id["id"])
-        print "Country id already there as " + str(country_id)
+        #print "Country id already there as " + str(country_id)
 
-    print "ugh"
     query = "INSERT INTO cars (make, model, year, country, type, hp, torque, miles_driven, acceleration, max_speed, added_by)" \
             "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    result = False
     try:
         result = db_conn.query_db(query, [make_id, model_id, int(cdata["year"]), country_id, int(cdata["ctype"]),
                                         int(cdata["hp"]), int(cdata["torque"]), int(cdata["odo"]),
                                         int(cdata["acceleration"]), int(cdata["max_speed"]), current_user.db_id],
                                         select=False)
     except Exception, e:
+        result = False
         print repr(e)
 
-    print result
-    print "added"
-
-    return True
+    return result is not False
